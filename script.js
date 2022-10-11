ab6f5c20f6264d097ae887e4b316e5dde63d671e
gltats <tatianasofiagomes831@gmail.com>
2022-10-10 22:44:15 -0600

    timeout dicreased

--- script.js
+++ script.js
@@ -8,8 +8,26 @@ $(function() {
     $(".no").on({
       mouseover: function() {
         $(this).css({
-          left: (Math.random() * 200) + "px",
-          top: (Math.random() * 200) + "px",
+          left: (Math.random() * 800) + "px",
+          top: (Math.random() * 600) + "px",
+        });
+      }
+    });
+  });
+
+const btn = document.getElementById('no');
+btn.addEventListener('click', function handleClick(){
+  btn.textContent = 'YOU MADE ALL THE RABBIT COMUNITY SAD!😫';
+  setTimeout(1000);
+});
+
+
+$(function() {
+    $(".no").on({
+      onclick: function() {
+        $(this).css({
+          left: (Math.random() * 800) + "px",
+          top: (Math.random() * 600) + "px",
         });
       }
     });
